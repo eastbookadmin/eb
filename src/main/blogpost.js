4bc1c78ee826174c11618e5ae05ff07428333468
Jay <jay@192.168.1.112>
2022-01-22 10:59:10 +0300

    project init

---
+++ src/main/blogpost.js
@@ -0,0 +1,52 @@
+import react from "react";
+import "./index.css";
+
+function BlogContents() {
+  const [company_db, setCompany_db] = React.useState([]);
+  React.useEffect(function () {
+    axios
+      .get(
+        "https://343f1e8e-b631-4d85-a997-c3e4912f25bc.mock.pstmn.io/companydb"
+      )
+      .then(function (result) {
+        const company_db = result.data.company_db;
+        setCompany_db(company_db);
+      })
+      .catch(function (error) {
+        console.error("error :", error);
+      });
+  }, []);
+
+  return (
+    <div className="contents-list">
+      {company_db.map(function (content, index) {
+        return (
+          <div className="content_card">
+            <h2>{content.company_name}</h2>
+            <p>{content.about}</p>
+            <img
+              className="contents_media"
+              src="images/icons/img_thumb@2x.png"
+            />
+            <img className="author_company" src="images/logo/wato.jpg" />
+            <span>{content.author_company}</span>
+            <br />
+            <img
+              className="post_footer_icon"
+              src="images/icons/ic_count_save_12px.svg"
+            />
+            <span className="post_footer_info">saved_num</span>
+            <img
+              className="post_footer_icon"
+              src="images/icons/ic_count_comment_12px.svg"
+            />
+            <span className="post_footer_info">comment_num</span>
+            <span className="post_footer_info">date</span>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default BlogContents;
